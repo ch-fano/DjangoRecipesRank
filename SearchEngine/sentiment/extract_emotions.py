414d6ea7af06c98054891ab544398aca5ff4054f
lollo <lollo.taccini@gmail.com>
2024-08-25 17:00:54 +0200

    added sentiment files, requirements.txt updated with all necessary new packages, please run a pip install -r requirements.txt before running

---
+++ SearchEngine/sentiment/extract_emotions.py
@@ -0,0 +1,27 @@
+from transformers import RobertaTokenizer, AutoModelForSequenceClassification, pipeline
+
+class ExtractEmotions:
+
+    def __init__(self):
+        self.tokenizer = RobertaTokenizer.from_pretrained(f"j-hartmann/emotion-english-distilroberta-base")
+        self.model = AutoModelForSequenceClassification.from_pretrained(f"j-hartmann/emotion-english-distilroberta-base")
+
+    def extract(self, text):
+        self.create_pipeline()
+        return self.emotions(text)
+    
+    def create_pipeline(self):
+        self.emotions = pipeline(
+            model=self.model,
+            tokenizer=self.tokenizer,
+            task="text-classification",
+            top_k=7,
+            max_length=512, 
+            truncation=True
+        )
+
+if __name__ == "__main__":
+    classifier = ExtractEmotions()
+    text = "What a wonderful accomodation! I loved staying there. Although people outside were a little bit scary, the stay was pleasant."
+    results = classifier.extract(text)
+    print(results)
---
+++ SearchEngine/sentiment/sentiment_model.py
@@ -0,0 +1,73 @@
+from whoosh.scoring import BM25F
+import math
+from SearchEngine.sentiment.reviews import ReviewsIndex
+from SearchEngine.sentiment.extract_emotions import ExtractEmotions
+
+
+class SentimentModelWA(BM25F):  # Sentiment Model Weighted Average
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_sentiment = None
+        self.reviews_index = ReviewsIndex()
+        self.use_final = True
+        
+    
+    def cosine_similarity(self, doc: dict, query: dict):
+        '''
+        Calcola cosine similarity.
+        '''
+        #denominatore
+        d_norm = math.sqrt(sum(v**2 for v in doc.values()))                 #radice norma documenti ritrovati
+        q_norm = math.sqrt(sum(v**2 for v in query.values()))               #radice norma query
+        denom = (d_norm * q_norm)                                           
+        
+        #numeratore
+        num = sum(doc[k]*query[k] for k in (doc.keys() & query.keys()))
+        if denom:
+            return num/denom
+        return 0
+    
+    def get_sentiment_score(self, listing_id, sentiment):
+        '''
+        Cosine similarity tra: 
+            - sentimenti di un certo documento
+            - sentimenti query
+        '''
+        return self.cosine_similarity(self.reviews_index.get_sentiments(int(listing_id)), sentiment)
+    
+
+    def set_user_sentiment(self, user_sentiment: list=None):
+        '''
+        Imposta i sentimenti in un dizionario dove le chiavi sono i sentimenti. 
+        '''
+        if user_sentiment:
+            self.user_sentiment = {k: 1 for k in user_sentiment}
+        else:
+            self.user_sentiment = None
+
+
+    def final(self, searcher, docnum, score):
+        
+        score = super().final(searcher, docnum, score)
+                            
+        if not self.user_sentiment:
+            return score
+
+        id = searcher.stored_fields(docnum)['id']
+        sentiment_score = self.get_sentiment_score(id, self.user_sentiment)
+        
+        return ((score/30*70)+(sentiment_score*30))/2
+ 
+class SentimentModelARWA(SentimentModelWA): # Sentiment Model Amount Review - Weighted Average
+    '''
+    Modello che differisce dal primo poichè premia i documenti con più recensioni penalizzando
+    pesantemente quelli che ne hanno poche. 
+    '''
+    def final(self, searcher, docnum, score):
+        score = super().final(searcher, docnum, score)
+        if not self.user_sentiment:
+            return score
+        id = searcher.stored_fields(docnum)['id']
+        sentiment_score = self.get_sentiment_score(id, self.user_sentiment)
+        
+        return ((score/30*70)+(sentiment_score*20)+(self.reviews_index.get_sentiment_len_for(id)*10))/3
