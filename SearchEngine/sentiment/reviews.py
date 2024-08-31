18fa87eaf93f06e8273b74d17b008e7146e3701a
Manu <emanuele.s.reggiani@gmail.com>
2024-08-31 17:47:48 +0200

    bugfix: file path resolved

--- SearchEngine/sentiment/reviews.py
+++ SearchEngine/sentiment/reviews.py
@@ -5,6 +5,8 @@ from SearchEngine.sentiment.extract_emotions import ExtractEmotions
 import yaml
 import os
 
+from costants import BASE_DIR
+
 
 def process_reviews(recipe_id, list_reviews, SENTIMENTS, sentiment):
     local_sentiments = {s: 0 for s in SENTIMENTS}
@@ -19,21 +21,23 @@ def process_reviews(recipe_id, list_reviews, SENTIMENTS, sentiment):
 
 class ReviewsIndex:
     def __init__(self, force_build_index=False):
-        with open('./SearchEngine/config.yaml', 'r') as file:
+        config_path = os.path.join(BASE_DIR, 'SearchEngine' ,'config.yaml')
+        with open(config_path, 'r') as file:
             self.config_data = yaml.safe_load(file)
 
-        with open('./SearchEngine/dataset/review.pkl', 'rb') as file:
+        review_path = os.path.join(BASE_DIR, 'SearchEngine' ,'dataset', 'review.pkl')
+
+        with open(review_path, 'rb') as file:
             self.review_dict = pickle.load(file)
 
-        path = f"./{self.config_data['INDEX']['SENTIMENT']}"
-        if not os.path.exists(path) or force_build_index:
+        index_path =os.path.join(BASE_DIR, self.config_data['INDEX']['SENTIMENT'])
+        if not os.path.exists(index_path) or force_build_index:
             self.setupReviewDB()
 
-        with open(path, "rb") as fp:
+        with open(index_path, 'rb') as fp:
             self.index = pickle.load(fp)
 
-
-    def setupReviewDB(self):
+    def setupReviewDB(self, index_path):
         print("Building reviews pickle file... ")
         SENTIMENTS = ["anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"]
 
@@ -43,15 +47,16 @@ class ReviewsIndex:
         tot_recipes = len(self.review_dict)
 
         with ProcessPoolExecutor() as executor:
-            futures = {executor.submit(process_reviews, recipe_id, list_reviews, SENTIMENTS, sentiment): recipe_id for recipe_id, list_reviews in self.review_dict.items()}
+            futures = {executor.submit(process_reviews, recipe_id, list_reviews, SENTIMENTS, sentiment): recipe_id for
+                       recipe_id, list_reviews in self.review_dict.items()}
             for future in tqdm(as_completed(futures), total=tot_recipes, desc="Indexing Recipes"):
                 recipe_id, sentiments = future.result()
                 sentiment_dict[recipe_id] = sentiments
 
-        with open(f"./{self.config_data['INDEX']['SENTIMENT']}", mode='wb') as f:
+        with open(index_path, mode='wb') as f:
             pickle.dump(sentiment_dict, f)
 
-        print("Review pickle file created successfully! ")
+        print("Review pickle file created successfully!")
 
     def get_sentiments(self, recipe_id):
         return self.index.get(recipe_id)
---
+++ costants.py
@@ -0,0 +1,8 @@
+# constants.py
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+
+if __name__ == "__main__":
+    print(BASE_DIR)
